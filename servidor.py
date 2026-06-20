0d182ffd59aba53c8df33d630b26f128544b8671
Alexis <alexis.gonzalescs@gmail.com>
2026-06-19 19:23:53 -0500

    servidor completo

--- servidor.py
+++ servidor.py
@@ -7,7 +7,19 @@ print ("Servidor encendido en el puerto 8080. Esperando...")
 
 while True:
     conexion, direccion = mi_socket.accept()
-    print('Conexion establecida, la direccion es', direccion)
-    mensaje = "Hola amix"
-    conexion.send(mensaje.encode('utf-8'))
-    conexion.close()
+    print(f"Conexion establecida, la direccion: {direccion}")
+    nombre_archivo = conexion.recv(1024).decode('utf-8').strip()
+    if not nombre_archivo:
+        conexion.close()
+        continue
+    print(f"El cliente ha solicitado leer:{nombre_archivo}")
+    if os.path.exists(nombre_archivo):
+        with open(nombre_archivo, 'r', encoding='utf-8') as archivo:
+            contenido = archivo.read()
+
+        conexion.send(contenido.encode('utf-8'))
+        print("Archivo enviado con éxito")
+    else:
+        mensaje_error = f"ERROR: El archivo '{nombre_archivo}' no existe en el servidor"
+        conexion.send(mensaje_error.encode('utf-8'))
+    conexion.close()        
